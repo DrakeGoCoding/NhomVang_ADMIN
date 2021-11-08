07bc9cf5269b9f863a66b4be9f71c56629f69e58
Dangeagle <tenanhladang1506@gmail.com>
2021-11-08 13:46:52 +0700

    main dashboard

---
+++ src/components/admin/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import Dashboard from "./dashboard";
+
+const Admin = () => {
+      return ( 
+            <Fragment>
+                  <Dashboard />
+            </Fragment>
+      )
+}
+export default Admin;
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Switch, Route, Redirect } from "react-router-dom";
 import Login from "../layout/login";
+import Admin from "../admin/index";
 const Section = () => {
     return (
         <>
             <Routes>
-                <Route exact path="/" element={<Login />} />
+                <Route exact path="/auth/login" element={<Login />} />
+                <Route path="/" element={<Admin />} />
             </Routes>
         </>
     );
