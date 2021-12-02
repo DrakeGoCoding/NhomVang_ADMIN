4d775fb1dff61d95b709139b2895b40120cc136b
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-12-02 18:56:15 +0700

    modify delete image api

--- src/api/image.api.js
+++ src/api/image.api.js
@@ -3,7 +3,7 @@ import { IMAGE_ENDPOINT } from "../constants/endpoints";
 
 const Image = {
     upload: image => agent.post(`${IMAGE_ENDPOINT}/upload`, { image }),
-    delete: id => agent.delete(`${IMAGE_ENDPOINT}`, { id })
+    delete: id => agent.delete(`${IMAGE_ENDPOINT}/${id}`)
 };
 
 export default Image;
