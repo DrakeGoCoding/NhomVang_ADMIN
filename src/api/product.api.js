9db120c3740243b4875bb83723147ef628ab35a1
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-12-08 10:33:00 +0700

    use params config

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -3,7 +3,6 @@ import { ADMIN_PRODUCT_ENDPOINT, PRODUCT_ENDPOINT } from "../constants/endpoints
 
 const pageSize = 10;
 
-const limit = (size, page) => `limit=${size}&offset=${page ? page * size : 0}`;
 const omitForProduct = product =>
     Object.assign({}, product, {
         slug: undefined,
@@ -13,7 +12,13 @@ const omitForProduct = product =>
 
 const Product = {
     getAll: (page = 0, filter = {}) =>
-        agent.get(`${PRODUCT_ENDPOINT}?${limit(pageSize, page)}&${new URLSearchParams(filter).toString()}`),
+        agent.get(PRODUCT_ENDPOINT, {
+            params: {
+                limit: pageSize,
+                offset: page * pageSize || 0,
+                ...filter
+            }
+        }),
     getBySlug: slug => agent.get(`${PRODUCT_ENDPOINT}/${slug}`),
     create: product => agent.post(ADMIN_PRODUCT_ENDPOINT, { product }),
     update: product =>
